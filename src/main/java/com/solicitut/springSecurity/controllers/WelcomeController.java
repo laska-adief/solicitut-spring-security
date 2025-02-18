93c68b5723615517e85b2867265348b56bfa2053
laska-adief <laskaadief.la@gmail.com>
2025-02-19 02:14:51 +0700

    create endpoint to get csrf token

--- src/main/java/com/solicitut/springSecurity/controllers/WelcomeController.java
+++ src/main/java/com/solicitut/springSecurity/controllers/WelcomeController.java
@@ -2,6 +2,8 @@ package com.solicitut.springSecurity.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 
 @RestController
 public class WelcomeController {
@@ -14,6 +16,11 @@ public class WelcomeController {
   @GetMapping("greet")
   public String greet() {
     return "Buenos Dias";
+  }
+
+  @GetMapping("csrf")
+  public CsrfToken getToken(HttpServletRequest request) {
+    return (CsrfToken) request.getAttribute("_csrf");
   }
 
 }
