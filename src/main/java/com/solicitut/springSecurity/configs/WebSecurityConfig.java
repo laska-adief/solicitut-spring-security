82b5902d60a5c20a440972c5fffdd56fd9e15354
laska-adief <laskaadief.la@gmail.com>
2025-02-19 02:39:07 +0700

    add config default authentication in WebSecurityConfig

--- src/main/java/com/solicitut/springSecurity/configs/WebSecurityConfig.java
+++ src/main/java/com/solicitut/springSecurity/configs/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.solicitut.springSecurity.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,7 +15,12 @@ public class WebSecurityConfig {
 //      All request will need to be authenticated, unauthenticated request will return 403 Forbidden
       .authorizeHttpRequests(authorizeRequests ->
         authorizeRequests.anyRequest().authenticated()
-      );
+      )
+//      Default Config authentication
+//      In Browser display popup to input username and password
+//      In Postman set the Authorization -> Auth Type (Basic Auth) -> set username and password
+//      Username and Password is based on that set in Spring Security Config in application.properties
+      .httpBasic(Customizer.withDefaults());
     return http.build();
   }
 }
