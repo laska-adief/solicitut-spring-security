0c155a719cd92d1af915c3d27f4c0e359d7d01cd
laska-adief <laskaadief.la@gmail.com>
2025-02-19 03:11:50 +0700

    create UserDetailsService for

--- src/main/java/com/solicitut/springSecurity/configs/WebSecurityConfig.java
+++ src/main/java/com/solicitut/springSecurity/configs/WebSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +33,20 @@ public class WebSecurityConfig {
 //      Instead of popup it will display form to input username and password
       .formLogin(Customizer.withDefaults());
     return http.build();
+  }
+
+//  UserDetailsService interface is used for retrieving user-related data for authentication
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails john = User.withUsername("john").password("john").roles("USER").build();
+    UserDetails alice = User.withUsername("alice").password("alice").roles("USER").build();
+
+//    Given that there is no default password encoder configured,
+//    each password must have a password encoding prefix.
+//    Please either prefix this password with '{noop}'
+//    or set a default password encoder in `DelegatingPasswordEncoder`.
+    UserDetails noop = User.withUsername("noop").password("{noop}noop").roles("USER").build();
+
+    return new InMemoryUserDetailsManager(john, alice, noop);
   }
 }
