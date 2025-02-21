378ab7af9526430c62cd0cb0dd37f5c046069c15
laska-adief <laskaadief.la@gmail.com>
2025-02-21 21:18:50 +0700

    create function findByUsername in UserRepository to connect data from database

--- src/main/java/com/solicitut/springSecurity/repositories/UserRepository.java
+++ src/main/java/com/solicitut/springSecurity/repositories/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsernameAndPassword(String username, String password);
+
+  User findByUsername(String username);
 }
