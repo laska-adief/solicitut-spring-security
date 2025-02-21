10b4ced90eae4653b8c2d4615b1f42e40a13da1f
laska-adief <laskaadief.la@gmail.com>
2025-02-21 20:37:08 +0700

    create endpoint login in AuthController

--- src/main/java/com/solicitut/springSecurity/controllers/AuthController.java
+++ src/main/java/com/solicitut/springSecurity/controllers/AuthController.java
@@ -18,4 +18,9 @@ public class AuthController {
   public User register(@RequestBody User user) {
     return userService.register(user);
   }
+
+  @PostMapping("login")
+  public User login(@RequestBody User user) {
+    return userService.login(user);
+  }
 }
